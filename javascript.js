43a62d86c1222b4e841deb2a5c4382c4d3c1af92
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-03 11:43:03 -0500

    feat: add initial files for working on javascript
    Worked through the coding assignment portion of the variable and
    operations

---
+++ javascript.js
@@ -0,0 +1,45 @@
+// Your JavaScript goes here!
+console.log("Testing this out")
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23 + 97 + 4 + 64 + 85 + 90);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+a = 13;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
