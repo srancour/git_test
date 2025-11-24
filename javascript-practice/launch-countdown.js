98116453a5d1a45c818e25f7991e45bf74a83007
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-24 17:50:11 -0500

    feat: for loop capturing countdown options
    10 will print different than 0 which is different from the rest

--- javascript-practice/launch-countdown.js
+++ javascript-practice/launch-countdown.js
@@ -2,6 +2,17 @@ const output = document.querySelector(".output");
 output.textContent = "";
 
 // let i = 10;
+for (let i = 10, i <= 0, i--) {
+    const para = document.createElement('p');
+    output.appendChild(para);
+    if (i === 10) {
+        para.textContent = `Countdown ${i}`;
+    } else if (i === 0) {
+        para.textContent = `Blast off!`;
+    } else {
+        para.textContent = `${i}`;
+    }
+}
 
 // const para = document.createElement('p');
 // para.textContent = ;
