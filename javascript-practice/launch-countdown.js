2b5050f000ec730753dec45b91328c1f6ac012f2
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-24 17:49:10 -0500

    feat: base files for launch countdown exercise

---
+++ javascript-practice/launch-countdown.js
@@ -0,0 +1,8 @@
+const output = document.querySelector(".output");
+output.textContent = "";
+
+// let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
