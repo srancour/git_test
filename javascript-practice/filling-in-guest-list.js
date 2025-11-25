aa58028f5bd0efd4c17251f7e8fa4762b84cd1c6
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-25 16:03:57 -0500

    fix: remove spacing

--- javascript-practice/filling-in-guest-list.js
+++ javascript-practice/filling-in-guest-list.js
@@ -8,24 +8,23 @@ const people = [
     "Sam",
     "Kay",
     "Bruce",
-  ];
-  
-  const admitted = document.querySelector(".admitted");
-  const refused = document.querySelector(".refused");
-  admitted.textContent = "Admit: ";
-  refused.textContent = "Refuse: ";
-  
-  // loop starts here
-  for (const person of people) {
-    if (person === "Phil" || person === "Lola") {
-        refused.textContent += `${person}, `;
-    } else {
-        admitted.textContent += `${person}, `;
-    }
-  }
+];
 
-  refused.textContent = `${refused.textContent.slice(0, -2)}.`; // Slice to final comma and space and replace with a period
-  admitted.textContent = `${admitted.textContent.slice(0, -2)}.`; // Slice to final comma and space and replace with a period
-  // refused.textContent += ...;
-  // admitted.textContent += ...;
-  
+const admitted = document.querySelector(".admitted");
+const refused = document.querySelector(".refused");
+admitted.textContent = "Admit: ";
+refused.textContent = "Refuse: ";
+
+// loop starts here
+for (const person of people) {
+if (person === "Phil" || person === "Lola") {
+    refused.textContent += `${person}, `;
+} else {
+    admitted.textContent += `${person}, `;
+}
+}
+
+refused.textContent = `${refused.textContent.slice(0, -2)}.`; // Slice to final comma and space and replace with a period
+admitted.textContent = `${admitted.textContent.slice(0, -2)}.`; // Slice to final comma and space and replace with a period
+// refused.textContent += ...;
+// admitted.textContent += ...;
