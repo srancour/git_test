c4f0d5c30d040828cf90828dbb020f3896905cc1
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-26 18:27:00 -0500

    feat: add fisher-yates shuffle to properly shuffle numbers

--- javascript-practice/array-methods/shuffle-array.js
+++ javascript-practice/array-methods/shuffle-array.js
@@ -5,7 +5,14 @@ let log = console.log;
 
 let arr = [1, 2, 3];
 
-
+function shuffle(array) {
+    // Fisher-Yates shuffle
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 log(shuffle(arr));
 log(shuffle(arr));
