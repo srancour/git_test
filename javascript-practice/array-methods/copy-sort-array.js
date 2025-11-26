77cdae42fbcebe33c16119fdcd2f328c9d1da88b
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-26 17:53:15 -0500

    feat: add initial files for copy and sort exercise

---
+++ javascript-practice/array-methods/copy-sort-array.js
@@ -0,0 +1,11 @@
+// Take in an array
+// Sort the array and save to new array
+
+let log = console.log;
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+log(sorted);
+log(arr);
