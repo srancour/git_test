6eefb7b8add526c63e8852f9525d8601b754aeb5
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-26 17:58:14 -0500

    feat: add function to sort a copy array

--- javascript-practice/array-methods/copy-sort-array.js
+++ javascript-practice/array-methods/copy-sort-array.js
@@ -5,6 +5,10 @@ let log = console.log;
 
 let arr = ["HTML", "JavaScript", "CSS"];
 
+function copySorted(arr) {
+    return arr.slice().sort(); // Slice creates a copy of the array and then sort that
+}
+
 let sorted = copySorted(arr);
 
 log(sorted);
