2e66d56e3aac78dc61ca5b063c85ec0bde11f95d
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-11-26 13:23:52 -0500

    feat: add function to filter values between 2 given values

--- javascript-practice/array-methods/filter-range.js
+++ javascript-practice/array-methods/filter-range.js
@@ -3,3 +3,15 @@
 // Return new array of all values that match
 let log = console.log;
 
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+function filterRange(arr, a, b) {
+    return arr.filter(
+        (num) => num >= a && num <= b
+    ); // take all values in the array greater than or equal to a and less than or equal to b and put them into a new array
+}
+
+log(arr);
+log(filtered);
