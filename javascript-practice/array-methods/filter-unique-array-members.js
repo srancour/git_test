09078e80cbcc6f2fd0722cb63d089bd312730782
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-01 13:56:29 -0500

    feat: add function to get unique elements

--- javascript-practice/array-methods/filter-unique-array-members.js
+++ javascript-practice/array-methods/filter-unique-array-members.js
@@ -4,9 +4,18 @@
 let log = console.log;
 
 function unique(arr) {
+    let result = []; // New array
+
+    for (let str of arr) { // for each string in the array
+        if(!result.includes(str)) { // if the string is not included in the result array
+            result.push(str); // add string to the result array
+        }
+    }
     
+    return result;
 }
 
 let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
 
 log(unique(strings));
+log(strings);
