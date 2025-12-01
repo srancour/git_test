359dc56035cab6895f392ce118ad4cbccb71f75b
Steve Rancour <18683379+srancour@users.noreply.github.com>
2025-12-01 13:54:08 -0500

    feat: initial files for filtering unique array

---
+++ javascript-practice/array-methods/filter-unique-array-members.js
@@ -0,0 +1,12 @@
+// Take in array
+// Create new array that contains only the unique elements from the original
+
+let log = console.log;
+
+function unique(arr) {
+    
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+log(unique(strings));
