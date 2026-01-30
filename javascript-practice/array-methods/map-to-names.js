3ee8c58887d1feb6633a4cd702cdd4557492dee3
Steve Rancour <18683379+srancour@users.noreply.github.com>
2026-01-30 17:45:32 -0500

    feat: add map to names exercise

---
+++ javascript-practice/array-methods/map-to-names.js
@@ -0,0 +1,20 @@
+let log = console.log;
+
+let john = {
+    name: "John", 
+    age: 25
+};
+let pete = {
+    name: "Pete", 
+    age: 30
+};
+let mary = {
+    name: "Mary", 
+    age: 28
+};
+
+let users = [john, pete, mary];
+
+let names = users.map(item => item.name);
+
+log(names);
